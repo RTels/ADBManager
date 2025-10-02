f279ce768917ffcd403b0e95866f2df679a406fc
Rafael Teles <rrft@cin.ufpe.br>
2025-10-02 13:08:34 -0300

    Initial Commit

---
+++ ADBManager/ADBManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADBManagerApp.swift
+//  ADBManager
+//
+//  Created by rrft on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ADBManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
